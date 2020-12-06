95504d1f4c9919851bb6f51bf99884e97ac7a622
rafasuman <rleonardomartinez@unicesar.edu.co>
2020-12-06 15:29:41 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Entidad;
+
+namespace Tienda
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Articulo articulo = new Articulo(10, "aceite", 1200);
+            int cantidad = 5;
+            Articulo articulo2 = new Articulo(10, "yuca", 4000);
+            int cantidad2 = 4;
+
+
+            Factura factura = new Factura();
+            factura.AddDetalle(articulo, cantidad);
+            factura.AddDetalle(articulo2, cantidad2);
+
+            foreach (var item in factura.Detalles)
+            {
+                Console.WriteLine(item.ToString());
+
+            }
+            Console.WriteLine(factura.ToString());
+        }
+    }
+}
